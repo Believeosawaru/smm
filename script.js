9af7a731bd7d17da413b6d8b9d7c2f8e4e2c9b4f
believe <believeosawaru2@gmail.com>
2024-07-21 14:15:28 +0100

    pusdh

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
-function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.querySelector('.to-top').style.display = 'block'
-    } else {
-        document.querySelector('.to-top').style.display = 'none'
-    }
-}
-
-window.onscroll = () => {
-    scrollFunction();
-}
-
-function headToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })
-}
+const menu = document.querySelector('.menu');
 
 function showMenu() {
-    const menu = document.querySelector('.menu');
 
-    if (menu.classList.contains('close-menu')) {
-        menu.classList.add('show-menu');
-
-        menu.classList.remove('close-menu');
-    } else {
+    if (menu.classList.contains('show-menu')) {
         menu.classList.add('close-menu');
-        
+
         menu.classList.remove('show-menu');
+    } else {
+        menu.classList.add('show-menu');
+        
+        menu.classList.remove('close-menu');
     }
 }
